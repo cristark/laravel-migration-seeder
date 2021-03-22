216b7960622168e4f7b8ff6b0d252705bc747803
Cristiano <cristiano.tarconi@gmail.com>
2021-03-22 16:16:23 +0100

    init + table migration

---
+++ database/migrations/2021_03_22_150612_create_houses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+            $table->string('street', 40)->unique();
+            $table->date('contruction_date');
+            $table->tinyInteger('mq');
+            $table->tinyInteger('rooms');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+}
